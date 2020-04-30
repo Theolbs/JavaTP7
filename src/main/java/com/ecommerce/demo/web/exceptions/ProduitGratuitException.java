03199e1cf72e3e7fb53b7b0d502cd77b18fcf1da
Lobinhes <theo.lobinhes@gmail.com>
2020-04-30 16:28:01 +0200

    Partie 5

--- src/main/java/com/ecommerce/demo/web/exceptions/ProduitGratuitException.java
+++ src/main/java/com/ecommerce/demo/web/exceptions/ProduitGratuitException.java
@@ -3,7 +3,7 @@ package com.ecommerce.demo.web.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ProduitGratuitException extends RuntimeException{
     public ProduitGratuitException(String s) {
         super(s);
